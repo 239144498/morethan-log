be01417b9bf88ab671620e9200800ea12dd383e7
Naihe <64341459+239144498@users.noreply.github.com>
2025-02-27 09:46:12 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "naihe",
-    image: "", // /avatar.jpg If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    // image: "", // /avatar.jpg If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "I develop everything using node.",
     email: "ww1998mail@gmail.com",
